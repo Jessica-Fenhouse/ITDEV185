da1e65afb95902351a5abb8d40b14a337aafe325
Jessica Fenhouse <Fenhousj@gmatc.matc.edu>
2020-02-24 18:09:08 -0600

    finished Assignment 4 Iterators

---
+++ IteratorsAssignment4/IteratorsAssignment4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IteratorsAssignment4
+{
+    class Program
+    {
+        public static IEnumerable<string> GetList()
+        {
+            List<string> list = new List<string>();
+            list.Add("Eggs");  
+            list.Add("Milk");
+            list.Add("Juice");
+            list.Add("Crackers");
+            list.Add("Sunflower Seeds");
+
+            foreach (var element in list)
+            {
+                yield return element;   
+            }
+        }
+
+        static void Main(string[] args)
+        { 
+            IEnumerable<string> elements = GetList();
+            foreach (var element in elements)  
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
